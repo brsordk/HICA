d98fd361e35ec6bb080c70c49ea9b8837f60396e
Baris Ördek <109208822+brsordk@users.noreply.github.com>
2023-12-05 14:38:40 +0100

    Add files via upload

--- HICA.m
+++ HICA.m
@@ -26,6 +26,15 @@ function HICA(Input,CN)
 % Version:  3.0.0
 % License: GNU General Public License v3.0
 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+
 arguments
     Input = 'Insert Image Directory Here';
     CN = ClusterNumber; %Specify the Cluster Number Here
